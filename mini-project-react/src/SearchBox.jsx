79f000757fc960f18c41ce21311fe720563c1396
Harman Singh <singh.harmanveer2003@gmail.com>
2025-06-18 18:04:23 +0530

    Moved API key to .env and updated .gitignore

--- mini-project-react/src/SearchBox.jsx
+++ mini-project-react/src/SearchBox.jsx
@@ -7,7 +7,7 @@ export default function SearchBox({updateInfo}){
     let[city,setCity]= useState("");
     let[error,setError]= useState(false);
     const API_URL="https://api.openweathermap.org/data/2.5/weather";
-    const API_KEY="5340bae8f596da05fdc15b46e4e37cec";
+    const API_KEY=process.env.REACT_APP_WEATHER_API_KEY;
 
     let getWeatherInfo=async()=>{
         try{
